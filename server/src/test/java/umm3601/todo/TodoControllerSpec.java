66863b25efc7bf257141a184c97935e87f930e4b
Jacob <jenne077@morris.umn.edu>
2021-03-01 16:38:53 -0600

    Create some tests for TodoControllerSpec
    Start writing the tests for the TodoController.
    Also delete three unnecessary imports from TodoController
    Co-authored-by: @Natasha-Zebrev

---
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -0,0 +1,108 @@
+package umm3601.todo;
+
+import static com.mongodb.client.model.Filters.eq;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.http.util.ContextUtil;
+import io.javalin.plugin.json.JavalinJson;
+
+/**
+ * Tests the logic of the TodoController
+ */
+public class TodoControllerSpec {
+
+  MockHttpServletRequest mockReq = new MockHttpServletRequest();
+  MockHttpServletResponse mockRes = new MockHttpServletResponse();
+
+  private TodoController todoController;
+
+  private ObjectId frysTodoId; // May need to change
+
+  static MongoClient mongoClient;
+  static MongoDatabase db;
+
+  static ObjectMapper jsonMapper = new ObjectMapper();
+
+  @BeforeAll
+  public static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(MongoClientSettings.builder()
+        .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr)))).build());
+
+    db = mongoClient.getDatabase("test");
+  }
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+
+    // Reset our mock request and response objects
+    mockReq.resetAll();
+    mockRes.resetAll();
+
+    // Setup database
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    // Something in VS Code is auto-formatting these code blocks
+    // every time that I save, so they do not look pretty.
+    testTodos.add(new Document().append("owner", "Fry").append("status", false).append("category", "software design")
+        .append("body", "This is a test of the body."));
+    testTodos.add(new Document().append("owner", "Blanche").append("status", true).append("category", "video games")
+        .append("body", "This is another test of the body, but with extra text."));
+    testTodos.add(new Document().append("owner", "Barry").append("status", true).append("category", "homework")
+        .append("body", "Yet another test of the body, but with even more extra text that goes even longer!"));
+
+    frysTodoId = new ObjectId();
+    Document fry = new Document().append("_id", frysTodoId).append("owner", "Fry").append("status", "true")
+        .append("category", "homework").append("body", "This is frysTodoId body text.");
+
+    todoDocuments.insertMany(testTodos);
+    todoDocuments.insertOne(fry);
+
+    todoController = new TodoController(db);
+  }
+
+  @AfterAll
+  public static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+
+  @Test
+  public void GetAllTodos() throws IOException {
+
+    // stub
+  }
+}
